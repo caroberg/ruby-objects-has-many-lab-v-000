0aa632df851458afd5e95a0238830997d54ad25f
Caroline Berg <carolineeberg@gmail.com>
2018-09-07 00:11:28 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,28 +4,28 @@ class Author
   @@title_count = 0
 
   def initialize(name)
-    @name = name 
+    @name = name
     @titles = []
-  end 
+  end
 
   def add_title(title)
-    self.titles << title 
-    title.author = self 
-    @@title_count +=1 
-  end 
+    self.titles << title
+    title.author = self
+    @@title_count +=1
+  end
 
   def add_title_by_name(name)
     title = Post.new(name)
-    self.titles << title 
-    title.author = self 
-    @@title_count +=1 
-  end 
+    self.titles << title
+    title.author = self
+    @@title_count +=1
+  end
 
-  def titles 
-    @titles 
-  end 
+  def titles
+    @titles
+  end
 
-  def self.title_count 
+  def self.title_count
     @@title_count
-  end 
+  end
 end
